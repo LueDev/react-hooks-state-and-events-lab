f281174f6c889d18a9f16cfed5b9184f4c805b3b
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-11-20 19:04:54 -0500

    All stateful variables were added along with conditionals and classLists to match the example gif. All tests passing.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import { setConfig } from "next/config";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+  function toggleCart(){
+    setInCart(!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={toggleCart}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
